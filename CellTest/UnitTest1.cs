00c7170485d86386b65d78f046ea03d83a32162f
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-09 22:25:07 -0600

    made SetUpLeft method

---
+++ CellTest/UnitTest1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConwaysGameOfLife;
+
+namespace CellTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanCreateInstanceOfCell()
+        {
+            Cell cell = new Cell(0, 0, true);
+            Assert.IsNotNull(cell);
+        }
+
+        [TestMethod]
+        public void CellsCanBeCreatedDead()
+        {
+            Cell cell = new Cell(0, 0, true);
+            Assert.AreEqual(true, cell.Dead);
+        }
+
+        [TestMethod]
+        public void CellsCanBeCreatedNotDead()
+        {
+            Cell cell = new Cell(0, 0, false);
+            Assert.AreEqual(false, cell.Dead);
+        }
+
+        [TestMethod]
+        public void TestUpLeft()
+        {
+            Cell cell = new Cell(0, 0, true);
+            cell.UpLeft = cell;
+            Cell expected = new Cell(-1, 1, true);
+            Assert.AreEqual(expected.X, cell.UpLeft.X);
+            Assert.AreEqual(expected.Y, cell.UpLeft.Y);
+        }
+
+        [TestMethod]
+        public void TestUp()
+        {
+            Cell cell = new Cell(0, 0, true);
+            cell.Up = cell;
+            Cell expected = new Cell(0, 1, true);
+            Assert.AreEqual(expected.X, cell.Up.X);
+            Assert.AreEqual(expected.Y, cell.Up.Y);
+        }
+
+        [TestMethod]
+        public void TestUpRight()
+        {
+            Cell cell = new Cell(0, 0, true);
+            cell.UpRight = cell;
+            Cell expected = new Cell(1, 1, true);
+            Assert.AreEqual(expected.X, cell.UpRight.X);
+            Assert.AreEqual(expected.Y, cell.UpRight.Y);
+        }
+
+        [TestMethod]
+        public void TestLeft()
+        {
+            Cell cell = new Cell(0, 0, true);
+            cell.Left = cell;
+            Cell expected = new Cell(-1, 0, true);
+            Assert.AreEqual(expected.X, cell.Left.X);
+            Assert.AreEqual(expected.Y, cell.Left.Y);
+        }
+
+        [TestMethod]
+        public void TestRight()
+        {
+            Cell cell = new Cell(0, 0, true);
+            cell.Right = cell;
+            Cell expected = new Cell(1, 0, true);
+            Assert.AreEqual(expected.X, cell.Right.X);
+            Assert.AreEqual(expected.Y, cell.Right.Y);
+        }
+
+        [TestMethod]
+        public void TestDownLeft()
+        {
+            Cell cell = new Cell(0, 0, true);
+            cell.DownLeft = cell;
+            Cell expected = new Cell(-1, -1, true);
+            Assert.AreEqual(expected.X, cell.DownLeft.X);
+            Assert.AreEqual(expected.Y, cell.DownLeft.Y);
+        }
+
+        [TestMethod]
+        public void TestDown()
+        {
+            Cell cell = new Cell(0, 0, true);
+            cell.Down = cell;
+            Cell expected = new Cell(0, -1, true);
+            Assert.AreEqual(expected.X, cell.Down.X);
+            Assert.AreEqual(expected.Y, cell.Down.Y);
+        }
+
+        [TestMethod]
+        public void TestDownRight()
+        {
+            Cell cell = new Cell(0, 0, true);
+            //Cell storage = new Cell(0, 0, true);
+            cell.DownRight = cell;
+            Cell expected = new Cell(1, -1, true);
+            Assert.AreEqual(expected.X, cell.DownRight.X);
+            Assert.AreEqual(expected.Y, cell.DownRight.Y);
+        }
+
+        [TestMethod]
+        public void TestSetUpLeft()
+        {
+            Cell cell = new Cell(0, 0, true);
+            cell.setUpLeft();
+            Cell expected = new Cell(-1, 1, true);
+            Assert.AreEqual(expected.X, cell.UpLeft.X);
+            Assert.AreEqual(expected.Y, cell.UpLeft.Y);
+
+            //Cell newCell = cell.UpLeft;
+            Assert.AreEqual(cell.UpLeft.DownRight.X, cell.X);
+            Assert.AreEqual(cell.UpLeft.DownRight.Y, cell.Y);
+        }
+
+        //[TestMethod]
+        //public void TestSetNeighbors()
+        //{
+        //    Cell cell = new Cell(0, 0, true);
+        //    cell.setNeighbors();
+        //    Cell expected = new Cell(-1, 1, true);
+        //    Assert.AreEqual(expected.X, cell.UpLeft.X);
+        //    Assert.AreEqual(expected.Y, cell.UpLeft.Y);
+        //    Cell expected2 = new Cell(0, 1, true);
+        //    Assert.AreEqual(expected2.X, cell.Up.X);
+        //    Assert.AreEqual(expected2.Y, cell.Up.Y);
+        //    Cell expected3 = new Cell(1, 1, true);
+        //    Assert.AreEqual(expected3.X, cell.UpRight.X);
+        //    Assert.AreEqual(expected3.Y, cell.UpRight.Y);
+        //    Cell expected4 = new Cell(-1, 0, true);
+        //    Assert.AreEqual(expected4.X, cell.Left.X);
+        //    Assert.AreEqual(expected4.Y, cell.Left.Y);
+        //    Cell expected5 = new Cell(1, 0, true);
+        //    Assert.AreEqual(expected5.X, cell.Right.X);
+        //    Assert.AreEqual(expected5.Y, cell.Right.Y);
+        //    Cell expected6 = new Cell(-1, -1, true);
+        //    Assert.AreEqual(expected6.X, cell.DownLeft.X);
+        //    Assert.AreEqual(expected6.Y, cell.DownLeft.Y);
+        //    Cell expected7 = new Cell(0, -1, true);
+        //    Assert.AreEqual(expected7.X, cell.Down.X);
+        //    Assert.AreEqual(expected7.Y, cell.Down.Y);
+        //    Cell expected8 = new Cell(1, -1, true);
+        //    Assert.AreEqual(expected8.X, cell.DownRight.X);
+        //    Assert.AreEqual(expected8.Y, cell.DownRight.Y);
+        //}
+    }
+}
---
+++ ConwaysGameOfLife/Cell.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    public class Cell
+    {
+        private int x;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        private int y;
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public Cell(int _x, int _y, bool _dead)
+        {
+            x = _x;
+            y = _y;
+            dead = _dead;
+        }
+
+        private bool dead;
+        public bool Dead
+        {
+            get { return dead;  }
+            set { dead = value; }
+        }
+
+        private Cell upLeft;
+        public Cell UpLeft
+        {
+            get { return upLeft; }
+            set { upLeft = new Cell(value.x - 1, value.y + 1, true); }
+        }
+
+        private Cell up;
+        public Cell Up
+        {
+            get { return up; }
+            set { up = new Cell(value.x, value.y + 1, true); }
+        }
+
+        private Cell upRight;
+        public Cell UpRight
+        {
+            get { return upRight; }
+            set { upRight = new Cell(value.x + 1, value.y + 1, true); }
+        }
+
+        private Cell left;
+        public Cell Left
+        {
+            get { return left; }
+            set { left = new Cell(value.x - 1, value.y, true); }
+        }
+
+        private Cell right;
+        public Cell Right
+        {
+            get { return right; }
+            set { right = new Cell(value.x + 1, value.y, true); }
+        }
+
+        private Cell downLeft;
+        public Cell DownLeft
+        {
+            get { return downLeft; }
+            set { downLeft = new Cell(value.x - 1, value.y - 1, true); }
+        }
+
+        private Cell down;
+        public Cell Down
+        {
+            get { return down; }
+            set { down = new Cell(value.x, value.y - 1, true); }
+        }
+
+        private Cell downRight;
+        public Cell DownRight
+        {
+            get { return downRight; }
+            set {
+                //if(this == value)
+                //{
+                    downRight = new Cell(value.x + 1, value.y - 1, true);
+                //}
+                //else
+                //{
+                //    throw new ArgumentException();
+                //}
+            }
+        }
+
+        public void setUpLeft()
+        {
+            UpLeft = this;
+            this.X += -1;
+            this.Y += 1;
+            UpLeft.DownRight = this;
+            this.X += 1;
+            this.Y += -1;
+        }
+        //public void setNeighbors()
+        //{
+        //    UpLeft = this;
+        //    Up = this;
+        //    UpRight = this;
+        //    Left = this;
+        //    Right = this;
+        //    DownLeft = this;
+        //    Down = this;
+        //    DownRight = this;
+        //}
+
+        //if(Up!==null){create;}
+    }
+}

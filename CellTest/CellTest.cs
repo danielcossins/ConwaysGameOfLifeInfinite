1140067483a3d6fe1999468b08248676baf1eb58
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-11 13:57:02 -0600

    infinite board first tests

---
+++ CellTest/CellTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConwaysGameOfLife;
+
+namespace CellTest
+{
+    [TestClass]
+    public class CellTest
+    {
+        [TestMethod]
+        public void CellEnsureCellIsAlive()
+        {
+            // Object Initializer Syntax
+            // Use Public setters when creating new instance
+            Cell my_cell = new Cell { IsAlive = true };
+            Assert.IsTrue(my_cell.IsAlive);
+        }
+
+        [TestMethod]
+        public void CellEnsureCellIsDeadByDefault()
+        {
+            Cell my_cell = new Cell();
+            Assert.IsFalse(my_cell.IsAlive);
+        }
+
+        [TestMethod]
+        public void CellEnsureCellHasAPosition()
+        {
+            Cell cell = new Cell();
+            Assert.AreEqual(0, cell.X);
+            Assert.AreEqual(0, cell.Y);
+        }
+
+        [TestMethod]
+        public void CellEnsureWeCanProvidePosition()
+        {
+            int x = 1;
+            int y = 2;
+            Cell cell = new Cell { X = x, Y = y };
+            Assert.AreEqual(x, cell.X);
+            Assert.AreEqual(y, cell.Y);
+        }
+    }
+}
---
+++ CellTest/InfiniteBoardTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConwaysGameOfLife;
+
+namespace CellTest
+{
+    [TestClass]
+    public class InfiniteBoardTest
+    {
+        [TestMethod]
+        public void InfiniteBoardEnsureICanCreateInstance()
+        {
+            InfiniteBoard world = new InfiniteBoard();
+            Assert.IsNotNull(world);
+        }
+
+        [TestMethod]
+        public void InfiniteBoardEnsureTheWorldHasZeroCells()
+        {
+            InfiniteBoard world = new InfiniteBoard();
+            int expectedNumberOfCells = 0;
+            int actualNumberOfCells = world.CellCount();
+
+            Assert.AreEqual(expectedNumberOfCells, actualNumberOfCells);
+        }
+
+        [TestMethod]
+        public void InfiniteBoardEnsureThereAreCells()
+        {
+            InfiniteBoard world = new InfiniteBoard();
+
+            int x = 4;
+            int y = 3;
+            world.AddCell(x, y);
+
+            Assert.AreEqual(1, world.CellCount());
+        }
+    }
+}
